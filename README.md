463a1c841f598f0d675a56daf9d79d56a3245882
joao igor <joaoigormatos123@gmail.com>
2021-11-27 17:07:06 -0300

    Add a README.md file and swagger support

---
+++ README.md
@@ -0,0 +1,21 @@
+# Manager
+
+This repo is meant to be the backend for the eback project.
+
+#### To install all the depedency just run:
+
+```
+$ npm install
+```
+
+#### To build the project just run:
+
+```
+$ npm run build
+```
+
+#### To deploy the project on local env just run:
+
+```
+$ npm run dev
+```
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerConfig from "./config/swagger.json";
+
 import { todoController, userController } from "./controllers";
 import authMiddleware from "./middlewares/auth";
 const router = express.Router();
@@ -13,6 +16,9 @@ router.delete("/user/todo/:id", authMiddleware, todoController.delete);
 router.get("/user/todo/:id", authMiddleware, todoController.fetchOne);
 router.get("/user/todo/", authMiddleware, todoController.fetchAll);
 router.put("/user/todo/:id", authMiddleware, todoController.update);
+
+//Documentation
+router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 router.use("/", (_: Request, res: Response) => {
   res.status(200).json({
